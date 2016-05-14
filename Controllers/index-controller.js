6b13bf9aeecb27a5072ab4b90da1b08c43640310
Flávio da Maia Jr <flaviodamaiajr@gmail.com>
2016-05-14 17:02:38 -0300

    AngularJS
    Adicionado comentários na IndexController a qual herda as funções da
    BaseController.

--- Controllers/index-controller.js
+++ Controllers/index-controller.js
@@ -5,19 +5,21 @@
 
     function IndexController($log, $timeout, $controller) {
 
-        var ctrl = this;
-
+        var ctrl = this;        
+        // Criada a instância da controller Base.
         ctrl.base = $controller('BaseController', {});
 
         ctrl.year = new Date().getFullYear();        
-
+        
         ctrl.sayHello = function (button) {
 
             if(button == "index"){
+                // Utilizando a função customMessage da BaseController.
                 ctrl.base.customMessage("Sou a função da <strong>IndexController</strong>, utilizando a <strong>BaseController</strong>! <i class='fa fa-2x fa fa-hand-peace-o' aria-hidden='true'></i>", "Hey!");
                 $log.info("Say Hello - IndexController");
                 return;
             }
+            // Chamando a função sayHallo da BaseController.
             ctrl.base.sayHello();
 
         }
